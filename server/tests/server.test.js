10edccaa15dbe35f1a1ad732056f960ad8e09f25
andreip <andrei.popescu@infoworld.ro>
2017-10-13 15:01:11 +0300

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -266,3 +266,23 @@ describe('Post /users/login', ()=>{
         .end(done);
     })
 });
+describe('DELETE /users/me/token', ()=>{
+    it('should remove auth token on logout', (done)=>{
+        request(app)
+        .delete('/users/me/token')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .end((err, res) =>{
+            if(err){
+                return done(err);
+            }
+            User.findById(users[0]._id).then((user) =>{
+                expect(user.tokens.length).toBe(0);
+                done();
+            }).catch((e) =>{
+                return done(e);
+            });
+        });
+    
+    });
+})
